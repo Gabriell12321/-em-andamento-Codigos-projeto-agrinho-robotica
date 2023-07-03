317cdac67769f3ec38207aeb559e6b89e3ea804a
Gabriell12 <111468239+Gabriell12321@users.noreply.github.com>
2023-07-03 01:48:37 -0300

    Add files via upload

---
+++ AGRINHO2023/AGRINHO2023.ino
@@ -0,0 +1,35 @@
+
+
+ 
+#define pinoAnalog A0
+#define pinoRele 8 
+#define pino5V 7 
+ 
+int ValAnalogIn; 
+ 
+void setup() {
+Serial.begin(9600);
+Serial.println("www.usinainfo.com.br"); 
+pinMode(pinoRele, OUTPUT); 
+pinMode(pino5V, OUTPUT); 
+digitalWrite(pino5V, HIGH); 
+}
+ 
+void loop() {
+ValAnalogIn = analogRead(pinoAnalog); 
+int Porcento = map(ValAnalogIn, 1023, 0, 0, 100); 
+ 
+Serial.print(Porcento); 
+Serial.println("%"); 
+ 
+if (Porcento <= 45) { 
+Serial.println("Irrigando a planta ..."); 
+digitalWrite(pinoRele, HIGH); 
+}
+ 
+else { // Se não ...
+Serial.println("Planta Irrigada ..."); 
+digitalWrite(pinoRele, LOW); // 
+}
+delay (1000); // Estabelece o tempo de 1s para reinicializar a leitura
+}
